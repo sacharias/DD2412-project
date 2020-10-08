d2c1aa5db216b8c880172b5f55de89fdddd85cc7
Sacharias Sjöqvist <sacharias.sjoqvist@gmail.com>
2020-10-08 16:06:03 +0200

    RandAugment implemented

---
+++ augment_example.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from torchvision.datasets import CIFAR10
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+import torchvision
+import torch
+
+from augment.rand_augment import RandAugmentTransform
+
+np.random.seed(0)
+torch.manual_seed(0)
+
+
+transform = transforms.Compose(
+    [
+        RandAugmentTransform(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ]
+)
+
+trainset = CIFAR10(root="./data", train=True, download=True, transform=transform)
+trainloader = DataLoader(trainset, batch_size=4, shuffle=False)
+
+def imshow(img):
+    img = img / 2 + 0.5
+    plt.imshow(np.transpose(img.numpy(), (1, 2, 0)))
+    plt.show()
+
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+imshow(torchvision.utils.make_grid(images))
+
