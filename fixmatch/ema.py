c1d4572daf9b4c1b7f6a1ec5ef94c357223a559c
harispoljo <34479922+harispoljo@users.noreply.github.com>
2020-10-14 13:36:39 +0200

    Create ema.py

---
+++ fixmatch/ema.py
@@ -0,0 +1,15 @@
+import copy
+
+class EMA():
+  def __init__(self, decay, model):
+      self.decay = decay
+      self.emamodel = copy.deepcopy(model)
+      self.emamodel.cuda()
+  
+  def update(self, model):
+    state_dict_emamodel = self.emamodel.state_dict()
+    #Jag är paranoid att gradienten kanske flödar igenom?
+    #torch.no_grad kanske gör det snabbare
+    with torch.no_grad():
+      for name, param in model.state_dict().items():
+        state_dict_emamodel[name] = decay * state_dict_emamodel[name]  + (1 - decay) * param
