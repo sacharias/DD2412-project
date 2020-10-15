ec47ba4bb690059f4ea0485a41ffc9563abd5c0e
Oskar Eriksson <oskaerik@gmail.com>
2020-10-15 15:11:38 +0200

    Add basic test saved script

---
+++ test_saved.py
@@ -0,0 +1,38 @@
+import torch
+from torch.utils.data import DataLoader
+import torchvision
+from torchvision import transforms
+from fixmatch.models import WRN
+
+PATH = 'net-00340.pt'
+
+testset = torchvision.datasets.CIFAR10(
+    root='./data',
+    train=False,
+    download=True,
+    transform=transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ]
+    )
+)
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+model = WRN(num_classes=10).to(device)
+model.load_state_dict(torch.load(PATH))
+
+testloader = DataLoader(testset, batch_size=64, shuffle=False, num_workers=4, pin_memory=True)
+
+correct = 0
+total = 0
+model.eval()
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data[0].to(device), data[1].to(device)
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
