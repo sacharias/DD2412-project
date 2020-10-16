76ff52ac2cb9c45d1b5b514bd40ddcad477c4ead
Oskar Eriksson <oskaerik@gmail.com>
2020-10-16 10:12:20 +0200

    Implement running without unlabeled set

--- fixmatch/__init__.py
+++ fixmatch/__init__.py
@@ -14,67 +14,72 @@ def save_weights(weight_dir, epoch, loss, net, optimizer, ema):
         }, os.path.join(weight_dir, f'net-{epoch:05d}.pt'))
         torch.save(ema, os.path.join(weight_dir, f'ema-{epoch:05d}.pt'))
 
-def train(net, labeled_loader, unlabeled_loader, train_optimizer, threshold, lambda_u, epochs, ema_model, device, log_file=None, weight_dir=None):
+def train(net, labeled_dataloader, unlabeled_dataloader, validation_dataloader, optimizer, threshold, lambda_u, epochs, ema_model, device, log_file=None, weight_dir=None):
     net.train()
     net.to(device)
     CrossEntropyLoss = nn.CrossEntropyLoss(reduction='mean')
     SoftMax = nn.Softmax(dim=1)
 
+    if unlabeled_dataloader is None:
+        unlabeled_dataloader = [None] * len(labeled_dataloader)
+
     # Save values for cosine learning rate decay
-    initial_lr = train_optimizer.param_groups[0]['lr']
-    total_steps = min(len(labeled_loader), len(unlabeled_loader)) * epochs
+    initial_lr = optimizer.param_groups[0]['lr']
+    total_steps = min(len(labeled_dataloader), len(unlabeled_dataloader)) * epochs
     current_step = 0
 
     # Write header to log file and save initial weights
     if log_file is not None:
         with open(log_file, 'w') as f:
-            f.write('epoch,loss,pseudoacc\n')
+            f.write('epoch,train_loss,val_loss,val_acc,pseudo_acc\n')
     save_weights(weight_dir=weight_dir,
                  epoch=0,
                  loss=-1,
                  net=net.state_dict(),
-                 optimizer=train_optimizer.state_dict(),
+                 optimizer=optimizer.state_dict(),
                  ema=ema_model.emamodel.state_dict())
 
     for epoch in range(1, epochs + 1):
         print('Current epoch:', epoch)
 
-        data_loader = zip(labeled_loader, unlabeled_loader)
+        data_loader = zip(labeled_dataloader, unlabeled_dataloader)
         total_loss, total_num, total_correct, total_accepted, train_bar = 0.0, 0, 0, 0, tqdm(data_loader)
 
         for labeled_batch, unlabeled_batch in train_bar:
-            x_l, y_l = labeled_batch[0].to(device),labeled_batch[1].to(device)
-            xW_u, xS_u, y_u = unlabeled_batch[0].to(device),unlabeled_batch[1].to(device),unlabeled_batch[2].to(device)
+            x_l, y_l = labeled_batch[0].to(device), labeled_batch[1].to(device)
+            if unlabeled_batch is not None:
+                xW_u, xS_u, y_u = unlabeled_batch[0].to(device), unlabeled_batch[1].to(device), unlabeled_batch[2].to(device)
 
             # Maybe we could cat the tensors then chunk the result
             logits_l = net(x_l)
-            logitsW_u = net(xW_u)
-            logitsS_u = net(xS_u)
+            if unlabeled_batch is not None:
+                logitsW_u = net(xW_u)
+                logitsS_u = net(xS_u)
 
             loss_l = CrossEntropyLoss(logits_l, y_l)
 
-            # I believe we need to stop the gradient here
-            predicitionW_u = SoftMax(logitsW_u).detach()
-            mask = predicitionW_u.ge(threshold)
+            loss_u, unlabeled_samples_accepted, correct_pseudolabels = 0, 0, 0
+            if unlabeled_batch is not None:
+                # I believe we need to stop the gradient here
+                predicitionW_u = SoftMax(logitsW_u).detach()
+                mask = predicitionW_u.ge(threshold)
 
-            indices = mask.sum(dim=1).nonzero().flatten()
-            unlabeled_samples_accepted = len(indices)
-            pseudolabel = predicitionW_u.argmax(dim=1)
+                indices = mask.sum(dim=1).nonzero().flatten()
+                unlabeled_samples_accepted = len(indices)
+                pseudolabel = predicitionW_u.argmax(dim=1)
 
-            if unlabeled_samples_accepted > 0:
-                loss_u = CrossEntropyLoss(logitsS_u[indices,:], pseudolabel[indices])
-            else:
-                loss_u = 0
+                if unlabeled_samples_accepted > 0:
+                    loss_u = CrossEntropyLoss(logitsS_u[indices,:], pseudolabel[indices])
+
+                correct_pseudolabels = (pseudolabel[indices] == y_u[indices]).sum()
 
             loss = loss_l + lambda_u * loss_u
 
-            train_optimizer.zero_grad()
+            optimizer.zero_grad()
             loss.backward()
-            train_optimizer.step()
+            optimizer.step()
 
             ema_model.update(net)
-
-            correct_pseudolabels = (pseudolabel[indices] == y_u[indices]).sum()
 
             total_samples = y_l.size()[0] + unlabeled_samples_accepted
 
@@ -86,22 +91,32 @@ def train(net, labeled_loader, unlabeled_loader, train_optimizer, threshold, lam
 
             # Cosine learning rate decay
             lr = initial_lr * math.cos(7 * math.pi * current_step / (16 * total_steps))
-            for group in train_optimizer.param_groups:
+            for group in optimizer.param_groups:
                 group['lr'] = lr
             current_step += 1
 
-            train_bar.set_description('Loss: {:.4f}, Unlabeled samples: {}, Accuracy of pseudolabels: {:.4f}, LR: {:.4f}'.format(total_loss/total_num, unlabeled_samples_accepted, total_correct/(total_accepted+1e-20), lr))
+            train_bar.set_description('Train loss: {:.4f}, Unlabeled samples: {}, Accuracy of pseudolabels: {:.4f}, LR: {:.4f}'.format(total_loss/total_num, unlabeled_samples_accepted, total_correct/(total_accepted+1e-20), lr))
+
+        # Calculate val_loss/acc
+        total_val, correct_val, loss_val = 0, 0, 0.0
+        for x_val, y_val in validation_dataloader:
+            x_val, y_val = x_val.to(device), y_val.to(device)
+            logits_val = net(x_val)
+            loss_val += CrossEntropyLoss(logits_val, y_val)
+            _, predicted_val = torch.max(logits_val, 1)
+            total_val += y_val.size(0)
+            correct_val += (predicted_val == y_val).sum().item()
 
         # Save logs and weights
         if log_file is not None:
             with open(log_file, 'a') as f:
-                f.write(f'{epoch},{total_loss/total_num:.4f},{total_correct/(total_accepted+1e-20):.4f}\n')
-        if epoch % 10 == 0 and weight_dir is not None:
+                f.write(f'{epoch},{total_loss / total_num:.4f},{loss_val / total_val:.4f},{correct_val / total_val:.4f},{total_correct/(total_accepted+1e-20):.4f}\n')
+        if (epoch % 10 == 0 or epoch == epochs) and weight_dir is not None:
             save_weights(weight_dir=weight_dir,
                          epoch=epoch,
-                         loss=total_loss/total_num,
+                         loss=total_loss / total_num,
                          net=net.state_dict(),
-                         optimizer=train_optimizer.state_dict(),
+                         optimizer=optimizer.state_dict(),
                          ema=ema_model.emamodel.state_dict())
 
     return total_loss/total_num, total_correct/(total_accepted+1e-20)
--- run_train.py
+++ run_train.py
@@ -14,36 +14,31 @@ from fixmatch.ema import EMA
 
 # Parse arguments
 parser = ArgumentParser(description='Train using FixMatch.')
+parser.add_argument('--dataset', type=str, help='The dataset to use.', default='CIFAR-10')
 parser.add_argument('--labeled_size', type=int, help='Number of labeled samples.', default=4000)
-parser.add_argument('--validation_size', type=int, help='Number of validation samples.', default=10)
+parser.add_argument('--validation_size', type=int, help='Number of validation samples.', default=10000)
 parser.add_argument('--mu', type=int, help='There is a 1:mu ratio between labeled and unlabeled samples.', default=7)
 parser.add_argument('--batch_size', type=int, help='The size of a labeled batch.', default=64)
 parser.add_argument('--epochs', type=int, help='The number of epochs.', default=1024)
 parser.add_argument('--threshold', type=float, help='The confidence threshold for pseudo labels.', default=0.95)
 parser.add_argument('--lambda_u', type=float, help='The weight for the unlabled loss.', default=1.0)
 parser.add_argument('--decay', type=float, help='The EMA decay.', default=0.999)
-parser.add_argument('--seed', type=int, help='The random seed.', default=10)
-parser.add_argument('--history', type=str, help='The directory where the traiinng history should be saved.', default=f'history/train-{datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}')
+parser.add_argument('--seed', type=int, help='The random seed.', default=1337)
+parser.add_argument('--history', type=str, help='The directory where the training history should be saved.', default=f'history/train-{datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}')
 args = parser.parse_args()
 
-# Create datasets
-trainset = torchvision.datasets.CIFAR10(
-    root='./data',
-    train=True,
-    download=True,
-    transform=None
-)
-testset = torchvision.datasets.CIFAR10(
-    root='./data',
-    train=False,
-    download=True,
-    transform=transforms.Compose(
-        [
-            transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-        ]
+# Download dataset
+if args.dataset == 'CIFAR-10':
+    trainset = torchvision.datasets.CIFAR10(
+        root='./data',
+        train=True,
+        download=True,
+        transform=None
     )
-)
+else:
+    raise Exception(f'Dataset "{args.dataset}" is not implemented.')
+
+# Split dataset
 labeled_dataset, unlabled_dataset, validation_dataset = create_dataset_split(
     dataset=trainset,
     labeled_size=args.labeled_size,
@@ -53,7 +48,7 @@ labeled_dataset, unlabled_dataset, validation_dataset = create_dataset_split(
 
 # Create dataloaders
 labeled_dataloader = DataLoader(labeled_dataset, batch_size=args.batch_size, shuffle=True, num_workers=4, pin_memory=True)
-unlabled_dataloader = DataLoader(unlabled_dataset, batch_size=args.batch_size*args.mu, shuffle=True, num_workers=4, pin_memory=True)
+unlabled_dataloader = DataLoader(unlabled_dataset, batch_size=args.batch_size*args.mu, shuffle=True, num_workers=4, pin_memory=True) if args.mu > 0 else None
 validation_dataloader = DataLoader(validation_dataset, batch_size=args.batch_size, shuffle=False, num_workers=4, pin_memory=True)
 
 # Set device
@@ -68,7 +63,6 @@ info = '\n'.join([
     f'    Size of labeled dataset: {len(labeled_dataset)}',
     f'    Size of unlabeled dataset: {len(unlabled_dataset)}',
     f'    Size of validation dataset: {len(validation_dataset)}',
-    f'    Size of test dataset: {len(testset)}',
     f'    Training using device: {device}',
     f'    Saving history to: {args.history}',
     f'\nArgs: {vars(args)}'
@@ -82,5 +76,17 @@ model = WRN(num_classes=10).to(device)
 optimizer = optim.SGD(model.parameters(), lr=0.03, momentum=0.9, weight_decay=0.0005,  nesterov=True)
 ema_model = EMA(args.decay, model, device)
 
-train_loss, pseudolabel_acc = fixmatch.train(model, labeled_dataloader, unlabled_dataloader, optimizer, args.threshold, args.lambda_u, args.epochs, ema_model, device,
-                                             log_file=os.path.join(args.history, 'log.csv'), weight_dir=args.history)
+train_loss, pseudolabel_acc = fixmatch.train(
+    net=model,
+    labeled_dataloader=labeled_dataloader,
+    unlabeled_dataloader=unlabled_dataloader,
+    validation_dataloader=validation_dataloader,
+    optimizer=optimizer,
+    threshold=args.threshold,
+    lambda_u=args.lambda_u,
+    epochs=args.epochs,
+    ema_model=ema_model,
+    device=device,
+    log_file=os.path.join(args.history, 'log.csv'),
+    weight_dir=args.history
+)
