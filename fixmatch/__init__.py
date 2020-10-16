25a66abf0ee398977d97f69f7f53212c1a3ae34a
Oskar Eriksson <oskaerik@gmail.com>
2020-10-16 10:44:29 +0200

    Calculate val_loss less often

--- fixmatch/__init__.py
+++ fixmatch/__init__.py
@@ -97,29 +97,29 @@ def train(net, labeled_dataloader, unlabeled_dataloader, validation_dataloader, 
 
             train_bar.set_description('Train loss: {:.4f}, Unlabeled samples: {}, Accuracy of pseudolabels: {:.4f}, LR: {:.4f}'.format(total_loss/total_num, unlabeled_samples_accepted, total_correct/(total_accepted+1e-20), lr))
 
-        # Calculate val_loss/acc
-        net.eval()
-        with torch.no_grad():
-            total_val, correct_val, loss_val = 0, 0, 0.0
-            for x_val, y_val in validation_dataloader:
-                x_val, y_val = x_val.to(device), y_val.to(device)
-                logits_val = net(x_val)
-                loss_val += CrossEntropyLoss(logits_val, y_val)
-                _, predicted_val = torch.max(logits_val, 1)
-                total_val += y_val.size(0)
-                correct_val += (predicted_val == y_val).sum().item()
-        net.train()
-
         # Save logs and weights
-        if log_file is not None:
-            with open(log_file, 'a') as f:
-                f.write(f'{epoch},{total_loss / total_num:.4f},{loss_val / total_val:.4f},{correct_val / total_val:.4f},{total_correct/(total_accepted+1e-20):.4f}\n')
-        if (epoch % 10 == 0 or epoch == epochs) and weight_dir is not None:
+        if (epoch == 1 or epoch % 10 == 0 or epoch == epochs) and weight_dir is not None:
             save_weights(weight_dir=weight_dir,
                          epoch=epoch,
                          loss=total_loss / total_num,
                          net=net.state_dict(),
                          optimizer=optimizer.state_dict(),
                          ema=ema_model.emamodel.state_dict())
+            # Calculate val_loss/acc
+            net.eval()
+            with torch.no_grad():
+                total_val, correct_val, loss_val = 0, 0, 0.0
+                for x_val, y_val in validation_dataloader:
+                    x_val, y_val = x_val.to(device), y_val.to(device)
+                    logits_val = net(x_val)
+                    loss_val += CrossEntropyLoss(logits_val, y_val).item() * y_val.size(0)
+                    _, predicted_val = torch.max(logits_val, 1)
+                    total_val += y_val.size(0)
+                    correct_val += (predicted_val == y_val).sum().item()
+            net.train()
+        if log_file is not None:
+            with open(log_file, 'a') as f:
+                f.write(f'{epoch},{total_loss / total_num:.4f},{loss_val / total_val:.4f},{correct_val / total_val:.4f},{total_correct/(total_accepted+1e-20):.4f}\n')
+
 
     return total_loss/total_num, total_correct/(total_accepted+1e-20)
