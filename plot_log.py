766b909147734aa00d8095875e0e4b59a3954a7d
Oskar Eriksson <oskaerik@gmail.com>
2020-10-16 11:05:10 +0200

    Update log plotting script

--- plot_log.py
+++ plot_log.py
@@ -3,17 +3,17 @@ import matplotlib.pyplot as plt
 
 parser = ArgumentParser(description='Plot a csv log.')
 parser.add_argument('path', type=str, help='The path to the csv file.')
+parser.add_argument('x', type=str, help='The attribute to use as x.')
+parser.add_argument('y', type=str, help='The attribute to use as y.')
 args = parser.parse_args()
 
 with open(args.path, 'r') as f:
-    lines = f.read().splitlines()[1:]
-lines = [line.split(',') for line in lines]
-epochs, losses, pseudos = zip(*lines)
-epochs = [int(epoch) for epoch in epochs]
-losses = [float(loss) for loss in losses]
-pseudos = [float(pseudo) for pseudo in pseudos]
+    lines = f.read().splitlines()
 
-print(pseudos)
+d = dict()
+lines = [line.split(',') for line in lines]
+for entry in zip(*lines):
+    d[entry[0]] = [eval(e) for e in entry[1:]]
 
 def plot(x, y, xlabel, ylabel):
     plt.figure()
@@ -22,5 +22,4 @@ def plot(x, y, xlabel, ylabel):
     plt.ylabel(ylabel)
     plt.show()
 
-plot(epochs, pseudos, 'Epoch', 'Accuracy of pseudo labels')
-plot(epochs, losses, 'Epoch', 'Loss')
+plot(d[args.x], d[args.y], args.x, args.y)
