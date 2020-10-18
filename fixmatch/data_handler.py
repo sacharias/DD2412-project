3f7cf954bd877769222ed97b2d8ed88efacba85a
Oskar Eriksson <oskaerik@gmail.com>
2020-10-18 21:31:50 +0200

    Create different datasets for labeled/unlabeled/validation loaders to fix transformation bug

--- fixmatch/data_handler.py
+++ fixmatch/data_handler.py
@@ -26,21 +26,23 @@ strongly_augment = transforms.Compose(
     ]
 )
 
+normalize = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465),(0.2470, 0.2435, 0.2616))
+    ]
+)
 
-class unlabeled(Dataset):
-    def __init__(self, data, weaklyAug, stronglyAug):
+
+class Augmented(Dataset):
+    def __init__(self, data, *augmentations):
         self.data = data
-        self.weaklyAug = weaklyAug
-        self.stronglyAug = stronglyAug
+        self.augmentations = augmentations
 
     def __getitem__(self, index):
         x, y = self.data[index]
-
-        # For some reason it converts the image to a tensor,
-        # so have to convert it back to an PIL to apply transformation
-        img = transforms.ToPILImage()(x)
-        xW, xS = self.weaklyAug(img), self.stronglyAug(img)
-        return xW, xS, y
+        xAug = [augmentation(x) for augmentation in self.augmentations]
+        return (*xAug, y)
 
     def __len__(self):
         return len(self.data)
@@ -55,23 +57,14 @@ def create_dataset_split(dataset, labeled_size, validation_size):
 
     part1, part2, part3 = torch.utils.data.random_split(dataset, [labeled_size, len(dataset) - total_size, validation_size])
 
-    part1.dataset.transform = weakly_augment
-
-    labeled_dataset = part1
+    labeled_dataset = Augmented(part1, weakly_augment)
 
     # In the FixMatch paper they mention that they add the labeled dataset to the unlabeled dataset. However, because we want measure
     # the accuracy on the unlabeled samples, training on it would bias the metric, and therefore we don't add the labeled data.
     # However, to add the labeled data change this to the following line:
     # unlabled_dataset = unlabeled(ConcatDataset([part1, part2]), weakly_augment, strongly_augment)
-    unlabled_dataset = unlabeled(part2, weakly_augment, strongly_augment)
+    unlabled_dataset = Augmented(part2, weakly_augment, strongly_augment)
 
-    part3.dataset.transform = transforms.Compose(
-        [
-            transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465),(0.2470, 0.2435, 0.2616))
-        ]
-    )
-
-    validation_dataset = part3
+    validation_dataset = Augmented(part3, normalize)
 
     return labeled_dataset, unlabled_dataset, validation_dataset
