20991b9973add4331f0bc9c1c7efe719a8a569ca
Oskar Eriksson <oskaerik@gmail.com>
2020-10-19 14:45:41 +0200

    Fix bug with tqdm total

--- fixmatch/__init__.py
+++ fixmatch/__init__.py
@@ -36,7 +36,7 @@ def train(net, labeled_dataloader, unlabeled_dataloader, validation_dataloader, 
     while step < steps:
         print(f'Step {step}/{steps}')
 
-        train_bar = tqdm(zip(labeled_dataloader, unlabeled_dataloader), total=min(len(labeled_dataloader, len(unlabeled_dataloader))))
+        train_bar = tqdm(zip(labeled_dataloader, unlabeled_dataloader), total=min(len(labeled_dataloader), len(unlabeled_dataloader)))
         total_loss, total_loss_l, total_loss_u = 0.0, 0.0, 0.0
         total_correct, total_accepted, batches = 0, 0, 0
 
