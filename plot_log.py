703e191db50262eef633843cd065faa72dddc16f
Oskar Eriksson <oskaerik@gmail.com>
2020-10-22 14:23:04 +0200

    Add save and title to plot_log

--- plot_log.py
+++ plot_log.py
@@ -7,7 +7,9 @@ parser.add_argument('x', type=str, help='The attribute to use as x.')
 parser.add_argument('y', type=str, help='The attribute to use as y.')
 parser.add_argument('--xlabel', type=str, help='The label for the x-axis.')
 parser.add_argument('--ylabel', type=str, help='The label for the y-axis.')
+parser.add_argument('--title', type=str, help='The title for the figure.')
 parser.add_argument('--stepsize', type=int, help='The stepsize for the x and y lists.', default=1)
+parser.add_argument('--save', type=str, help='The file name to save as.')
 args = parser.parse_args()
 
 args.xlabel = args.x if args.xlabel is None else args.xlabel
@@ -26,6 +28,10 @@ def plot(x, y, xlabel, ylabel):
     plt.plot(x[0:-1:args.stepsize], y[0:-1:args.stepsize])
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
+    if args.title is not None:
+        plt.title(args.title)
+    if args.save is not None:
+        plt.savefig(args.save)
     plt.show()
 
 plot(d[args.x], d[args.y], args.xlabel, args.ylabel)
